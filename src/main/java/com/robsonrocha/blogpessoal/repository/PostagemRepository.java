92d1e8dc033618d30a9b53fc27627191e43ae50d
Robson A. Rocha <robson_it@outlook.com>
2023-12-02 22:01:27 -0300

    Implementação do CRUD de Postagem

--- src/main/java/com/robsonrocha/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/robsonrocha/blogpessoal/repository/PostagemRepository.java
@@ -1,8 +1,13 @@
 package com.robsonrocha.blogpessoal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import com.robsonrocha.blogpessoal.model.Postagem;
+import java.util.List;
+
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+	
 }
